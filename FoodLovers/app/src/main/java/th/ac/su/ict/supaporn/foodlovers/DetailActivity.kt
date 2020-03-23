4e18aad603dc41ac3731ae1454c2ad7059617e7f
Monybblue <57031253+Monybblue@users.noreply.github.com>
2020-03-23 21:08:26 +0700

    Update

---
+++ FoodLovers/app/src/main/java/th/ac/su/ict/supaporn/foodlovers/DetailActivity.kt
@@ -0,0 +1,41 @@
+package th.ac.su.ict.supaporn.foodlovers
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_detail.*
+import th.ac.su.ict.supaporn.foodlovers.R
+
+class DetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+
+        val foodName = intent.getStringExtra("foodName")
+        val description = intent.getStringExtra("description")
+        val imageFile = intent.getStringExtra("imageFile")
+        val detail = intent.getStringExtra("detail")
+        val price = intent.getStringExtra("price")
+        val reviewScore = intent.getStringExtra("reviewScore")
+
+
+        tvFoodName.setText(foodName)
+        //tvResturant.setText(price)
+        tvDetail1.setText(description)
+
+
+
+        tvResturant.setText(detail)
+
+
+        val res = resources
+        val drawableId:Int = res.getIdentifier(imageFile,"drawable",packageName)
+        val drawableId2:Int = res.getIdentifier(reviewScore,"drawable",packageName)
+        imgView.setImageResource(drawableId)
+        ratingView.setImageResource(drawableId2)
+
+
+//        val drawableRs:Int = res.getIdentifier(reviewScore,"drawable",packageName)
+//        ratingView.setImageResource(drawableRs)
+    }
+}
---
+++ FoodLovers/app/src/main/java/th/ac/su/ict/supaporn/foodlovers/data/Food.kt
@@ -0,0 +1,9 @@
+package th.ac.su.ict.supaporn.foodlovers.data
+data class Food(
+    val imageFile: String,
+    val foodName: String,
+    val detail: String,
+    val description: String,
+    val price: String,
+    val reviewScore: String
+)
